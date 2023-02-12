a354f639d7fed6c46b84da022283846b8a761cbe
Brad Coleman <bradcoleman60@gmail.com>
2023-02-11 19:54:58 -0800

    adds get all technology route and handlebar page to send all tech and badges and links to the page

---
+++ controllers/home-route.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const sequelize = require("../config/connection");
+
+const { Cohort, ProjectTechnologies, ProjectManager, Project, Technology, TeamMember } = require("../models");
+
+//Get all Technologies for Create New Project Flow - Check Box Area
+
+router.get("/", async (req, res) => {
+    try {
+        const techData = await Technology.findAll()
+
+        const techList = techData.map((data) => 
+        data.get ({plain: true}))
+
+        console.log("List of Tech", techList);
+
+        res.render("check-list", {techList});
+
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err)
+    }
+
+})
+
+module.exports = router;
