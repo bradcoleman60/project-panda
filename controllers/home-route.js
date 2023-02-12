b2663c002d8ce8bb7a16dde5ba8c17ff048a4509
Fayven <fayven.amelga@gmail.com>
2023-02-12 14:39:02 -0500

    Corrected reference error in get projects id

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,15 +2,7 @@ const router = require("express").Router();
 
 const sequelize = require("../config/connection");
 
-const {
-  Cohort,
-  ProjectTechnologies,
-  ProjectManager,
-  Project,
-  Technology,
-  TeamMember,
-  Technology1,
-} = require("../models");
+const {Cohort, ProjectTechnologies, ProjectManager, Project, Technology, TeamMember,Technology1} = require("../models");
 
 //Get all Technologies for Create New Project Flow - Check Box Area
 
@@ -40,14 +32,14 @@ router.get("/projects/:id", async (req, res) => {
       ],
     });
 
-    const projects = managerData.get({ plain: true });
+    const projects = projectData.get({ plain: true });
 
     res.render("projectList", {
       ...projects,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
-    // console.log(err);
+    console.log(err);
     res.status(500).json(err);
   }
 });
