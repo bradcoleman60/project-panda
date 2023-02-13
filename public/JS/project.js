440e74600080647a28448ca1ca7b2d0813e5e494
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-02-13 15:51:56 -0500

    Add new member to the database and add functionality to add technology button to display technologies page

--- public/JS/project.js
+++ public/JS/project.js
@@ -5,16 +5,17 @@ const newMemberForm = async (event) => {
 
     const full_name = document.querySelector('#full-name').value.trim();
     const github_username = document.querySelector('#github').value.trim();
-
+    console.log("full_name: ", full_name)
+    console.log("github_username: ", github_username)
     if (full_name && github_username) {
-        const response = await fetch('/api/project', {
+        const response = await fetch('/api/users/addMember', {
             method: 'POST',
             body: JSON.stringify({full_name, github_username}),
             headers: {'Content-Type': 'application/json'},
         });
-
+        // if user's full_name and github_username is provided, the Add member button will add them to the database and clears input fields
         if (response.ok) {
-            document.location.replace('/tech');
+            document.location.replace('/createMember');
         } else {
             alert(response.statusText);
         }
@@ -24,4 +25,16 @@ const newMemberForm = async (event) => {
 
 document
   .querySelector('#add-member-btn')
-  .addEventListener('submit', newMemberForm);
+  .addEventListener('click', newMemberForm);
+
+
+// when 'Add Technology' button is clicked, display technologies pages for user to choose
+document
+  .querySelector('#addTech')
+  .addEventListener('click', function(e){
+    e.preventDefault();
+    document.location.replace('/tech');
+  });
+
+
+
