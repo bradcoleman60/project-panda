29b96c6a11613c12ea673dc6a4e3d24cc1f8b541
Fayven <fayven.amelga@gmail.com>
2023-02-13 21:07:09 -0500

    Resolved merge conflicts

--- public/JS/login.js
+++ public/JS/login.js
@@ -5,7 +5,6 @@ const loginForm = async (event) => {
   const username = document.querySelector('#logUsername').value.trim();
   const password = document.querySelector('#logPassword').value.trim();
 
-<<<<<<< Updated upstream
   
   if (username && password) {
 
@@ -37,10 +36,6 @@ const registerForm = async (event) => {
     const regPassword = document.querySelector('#regPassword').value.trim();
   
     if (regUsername && regPassword) {
-=======
-  console.log(username, password);
-  if (username && password) {
->>>>>>> Stashed changes
       const response = await fetch('/api/users/login', {
           method: 'POST',
           body: JSON.stringify({username, password}),
@@ -57,38 +52,12 @@ const registerForm = async (event) => {
 
 
 
-// const registerForm = async (event) => {
-//     event.preventDefault();
-  
-//     const regUsername = document.querySelector('#regUsername').value.trim();
-//     const regPassword = document.querySelector('#regPassword').value.trim();
-  
-//     if (regUsername && regPassword) {
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ email, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/projects:id');
-//       } else {
-//         alert(response.statusText);
-//       }
-//     }
-//   };
   
   document
     .querySelector('#logBtn')
     .addEventListener('click', loginForm);
   
-<<<<<<< Updated upstream
   document
     .querySelector('#regBtn')
     .addEventListener('click', registerForm);
-=======
-//   document
-//     .querySelector('#regBtn')
-//     .addEventListener('click', registerForm);
->>>>>>> Stashed changes
   
